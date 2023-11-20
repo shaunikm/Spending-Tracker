806300f8afe8718efcf706d831f93a90672c506c
Shaunik Musukula <68877932+shaunikm@users.noreply.github.com>
2023-11-20 15:19:27 -0800

    Update settings

---
+++ settings.py
@@ -0,0 +1,7 @@
+# window dimensions
+WIDTH = 1000
+HEIGHT = 1000
+
+# window appearance
+DEFAULT_APPEARANCE = "system"
+DEFAULT_COLOR_THEME = "blue"
--- window.py
+++ window.py
@@ -7,8 +7,9 @@ ctk.set_default_color_theme(DEFAULT_COLOR_THEME)
 
 win = ctk.CTk()
 win.title("Spending Tracker")
+win.geometry(f"{WIDTH}x{HEIGHT}")
 
-button = ctk.CTkButton(master=win, text="Submit")
-button.place(relx=0.5, rely=0.5, anchor=CENTER)
+# button = ctk.CTkButton(master=win, text="Submit")
+# button.place(relx=0.5, rely=0.5, anchor=CENTER)
 
 win.mainloop()
