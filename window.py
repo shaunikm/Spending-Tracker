3ae9d2ab183631e083ba47a661fc0b3a6e62d4bd
Shaunik Musukula <68877932+shaunikm@users.noreply.github.com>
2023-11-20 14:33:35 -0800

    create base window

---
+++ window.py
@@ -0,0 +1,14 @@
+import customtkinter as ctk
+from tkinter import CENTER
+from settings import *
+
+ctk.set_appearance_mode(DEFAULT_APPEARANCE)
+ctk.set_default_color_theme(DEFAULT_COLOR_THEME)
+
+win = ctk.CTk()
+win.title("Spending Tracker")
+
+button = ctk.CTkButton(master=win, text="Submit")
+button.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+win.mainloop()
