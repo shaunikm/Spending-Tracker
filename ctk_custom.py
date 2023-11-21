5e34e659bd370a2588a6f9198c693acaaa50659d
Shaunik Musukula <68877932+shaunikm@users.noreply.github.com>
2023-11-20 19:30:04 -0800

    Fix issue with multiple buttons highlighting.

--- ctk_custom.py
+++ ctk_custom.py
@@ -7,7 +7,10 @@ class ButtonGroup():
     def add(self, button: CTkButton):
         self.buttons.append(button)
     
+    """   
     def make_clear(self, enter, leave):
+        print(self.buttons)
         for button in self.buttons:
             button.bind("<Enter>", lambda e: button.configure(fg_color=enter))
             button.bind("<Leave>", lambda e: button.configure(fg_color=leave))
+    """
--- window.py
+++ window.py
@@ -1,6 +1,7 @@
 from customtkinter import *
 from settings import *
 from ctk_custom import *
+from PIL import Image, ImageTk
 
 # appearance settings
 set_appearance_mode(DEFAULT_APPEARANCE)
@@ -16,15 +17,19 @@ win.geometry(f"{WIDTH}x{HEIGHT}")
 def home_pressed():
     print("Home")
 
-######################################################
+############################################################
+##                      Text Options                      ##
+############################################################
 
 # text options
 DEFAULT_FONT = CTkFont(
                 family="Futura",
-                size=20
+                size=25
                 )
 
-######################################################
+#######################################################
+##                      Sidebar                      ##
+#######################################################
 
 # sidebar dimensions
 sidebar = CTkFrame(win, width=WIDTH//5, height=HEIGHT, fg_color="#2b2b2b")
@@ -33,21 +38,26 @@ sidebar.grid(row=0, column=0)
 # disable scaling based on widgets in sidebar
 sidebar.grid_propagate(False)
 
-######################################################
-
 sidebar_buttons_list = [
-    CTkButton(master=sidebar, text="Home",
+    (CTkButton(master=sidebar, text="Home",
                 command=home_pressed, font=DEFAULT_FONT,
-                fg_color="#2b2b2b")
+                fg_color="#2b2b2b"), (0.1, 0.1)),
+    (CTkButton(master=sidebar, text="MyWallet",
+              font=DEFAULT_FONT,
+              fg_color="#2b2b2b"), (0.1, 0.2)),
+    (CTkButton(master=sidebar, text="Spending",
+              font=DEFAULT_FONT,
+              fg_color="#2b2b2b"), (0.1, 0.3))
 ]
 
 sidebar_buttons = ButtonGroup()
 for _ in sidebar_buttons_list:
-    _.place(relx=0.1, rely=0.1)
-    sidebar_buttons.add(_)
-sidebar_buttons.make_clear("#18345c", "#2b2b2b")
+    _[0].place(relx=_[1][0], rely=_[1][1])
+    sidebar_buttons.add(_[0])
 
-
+######################################################
+##                    Home Frame                    ##
+######################################################
 
 
 
