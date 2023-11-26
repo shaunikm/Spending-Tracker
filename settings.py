e8c5a27614bf69e40f4d996b01f623d6d7e1ca56
Shaunik Musukula <68877932+shaunikm@users.noreply.github.com>
2023-11-26 01:37:29 -0800

    Create Page class

--- settings.py
+++ settings.py
@@ -6,6 +6,12 @@ HEIGHT = 500
 DEFAULT_APPEARANCE = "dark"
 DEFAULT_COLOR_THEME = "dark-blue"
 
+# spacing
+GROUP_SPACING = 0.2
+HEADER_SPACING = 0.13
+LINE_SPACING = 0.11
+STARTING_UPPER_INDENT = 0.12
+
 # test money settings
 weekly_spending = 31.42
 weekly_budget = 60.76
